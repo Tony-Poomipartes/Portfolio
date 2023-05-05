7d43061d0c2968006962ee58a2090dff2f66b6b7
Tony-Poomipartes <tony.p.pro@gmail.com>
2023-05-05 02:11:59 +0200

    style: add the css , a profil picture , color theme switch and scroll side

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express();
 const router = require("./app/router/router");
 const middleware404 = require('./middlewares/middleware404');
+app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
 app.set("views", './app/views');
@@ -10,11 +11,12 @@ app.set("views", './app/views');
 const PORT = process.env.PORT || 3000;
 
 
-const path = require('path');
-const publicPath = path.resolve(__dirname, 'public');
-app.use(express.static(publicPath));
+// const path = require('path');
+// app.use(express.static(path.join(__dirname, './public')));
+// const publicPath = path.resolve(__dirname, 'public');
+// app.use(express.static(publicPath));
 // app.use(express.static(__dirname + './../public'));
-// app.use(express.static("./../public"));
+// app.use(express.static("public"));
 app.use(router);
 app.use(middleware404);
 app.listen(PORT, () => {
