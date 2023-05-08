acf26c94629e08a0958d0de7eca5df6427e46148
Tony-Poomipartes <tony.p.pro@gmail.com>
2023-05-08 23:26:44 +0200

    💄  style: fix responsive issues

--- index.js
+++ index.js
@@ -10,13 +10,6 @@ app.set("views", './app/views');
 
 const PORT = process.env.PORT || 3000;
 
-
-// const path = require('path');
-// app.use(express.static(path.join(__dirname, './public')));
-// const publicPath = path.resolve(__dirname, 'public');
-// app.use(express.static(publicPath));
-// app.use(express.static(__dirname + './../public'));
-// app.use(express.static("public"));
 app.use(router);
 app.use(middleware404);
 app.listen(PORT, () => {
